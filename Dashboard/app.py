4d37cd17fbe8d8ee25d64a72727b6c71ee9c05be
Weslan Silva <wesllan90@gmail.com>
2023-06-26 23:23:12 -0300

    finalizado dashboard com os dois graficos

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -48,7 +48,7 @@ app.layout = html.Div(
                 'padding': '1rem 5rem',
                 'width': '100%',
             },
-             children=[
+            children=[
 
                 html.Div(
                     className='banner-title',
@@ -69,7 +69,7 @@ app.layout = html.Div(
                         html.Img(src=app.get_asset_url('logo.webp'), height='30px', alt='Logo')
                     ]
                 )
-             ]
+            ]
         ),
 
         #Conteúdo da aplicação
@@ -82,114 +82,217 @@ app.layout = html.Div(
             },
 
            children=[
-                
-                html.Div(
-                    className='left-div',
-                    style={
-                        'display': 'flex',
-                        'flex-direction': 'column',
-                        'justify-content': 'space-evenly',
-                        'align-items': 'center',
-                        'margin': '0',
-                        'padding': '0',
-                        'width': '100%',
-                        'margin-bottom': '3rem',
-                        'background-color':'#161a28',
-                        'border': '#1e2130 solid 0.2rem' 
-                    },
 
-                    # Informações das Multas
+                html.Div(
+                    className='content',
+                    style={
+                        'display': 'grid', 
+                        'grid-template-columns': '20% 80%',
+                        'height' : '500px'
+                    },
+                    
                     children=[
 
                         html.Div(
-                            className='top',
+                            className='left-div',
+                            style={
+                                'display': 'flex',
+                                'flex-direction': 'column',
+                                'justify-content': 'space-evenly',
+                                'align-items': 'center',
+                                'margin': '0',
+                                'padding': '0',
+                                'width': '100%',
+                                'margin-bottom': '3rem',
+                                'background-color':'#161a28',
+                                'border': '#1e2130 solid 0.2rem' 
+                            },
+                            # Informações das Multas
+
                             children=[
-                                html.H5(
-                                    ['Total multas'], 
-                                    style={
-                                        'line-height': '1.6',
-                                        'box-sizing': 'border-box',
-                                        'margin': '1rem',
-                                        'font-weight': '500',
-                                        'align-self': 'flex-start',
-                                    }
+
+                                html.Div(
+                                    className='top',
+                                    children=[
+                                        html.H5(
+                                            ['Total multas'], 
+                                            style={
+                                                'line-height': '1.6',
+                                                'box-sizing': 'border-box',
+                                                'margin': '1rem',
+                                                'font-weight': '500',
+                                                'align-self': 'flex-start',
+                                            }),
+
+                                        html.H4(
+                                            [f'{ round( Base_Dados.Quantidade.sum() /1000000, 1 )  } mi'],
+                                            style={
+                                                'font-size': '1.5em',
+                                                'line-height': '1.6',
+                                                'font-weight': '400',
+                                                'color': 'orangered',
+                                                'border-radius': '3px',
+                                                'padding': '12px 8px 12px 14px',
+                                                'border': '1px solid #D3D3D3',
+                                                'background': '#1e2130',
+                                                'box-sizing': 'border-box',
+                                                'width': '100%',
+                                                'display': 'flex',
+                                                'flex-direction': 'row',
+                                                'justify-content': 'center',
+                                            }
+                                        )
+                                    ]
                                 ),
 
-                                html.H4(
-                                    [f'{ round( Base_Dados.Quantidade.sum() /1000000, 1 )  } mi'],
-                                    style={
-                                        'font-size': '1.5em',
-                                        'line-height': '1.6',
-                                        'font-weight': '400',
-                                        'color': '#b4bd53',
-                                        'border-radius': '3px',
-                                        'padding': '12px 8px 12px 14px',
-                                        'border': '1px solid #D3D3D3',
-                                        'background': '#1e2130',
-                                        'box-sizing': 'border-box',
-                                        'width': '100%',
-                                        'display': 'flex',
-                                        'flex-direction': 'row',
-                                        'justify-content': 'center',
-                                    },
+                                html.Div(
+                                    className='inner-div',
+                                    children=[
+                                        html.H5(
+                                            ['Total valores'], 
+                                            style={
+                                                'line-height': '1.6',
+                                                'box-sizing': 'border-box',
+                                                'margin': '1rem',
+                                                'font-weight': '500',
+                                                'align-self': 'flex-start',
+                                                
+                                            }
+                                        ),
+
+                                        html.H4(
+                                            [f'R$ { int( round( Base_Valor.Quantidade.sum() /1000000, 0 ) )  } mi'],
+                                            style={
+                                                'font-size': '1.5em',
+                                                'line-height': '1.6',
+                                                'font-weight': '400',
+                                                'color': '#3a8c5d',
+                                                'border-radius': '3px',
+                                                'padding': '12px 8px 12px 14px',
+                                                'border': '1px solid #D3D3D3',
+                                                'background': '#1e2130',
+                                                'box-sizing': 'border-box',
+                                                'width': '100%',
+                                                'display': 'flex',
+                                                'flex-direction': 'row',
+                                                'justify-content': 'center',
+                                            }
+                                        )
+                                    ]
                                 )
-                            ],
+                            ]
                         ),
 
                         html.Div(
-                            className='inner-div',
+
+                            className='right-div',
+                            style={
+                                'display': 'flex',
+                                'flex-direction': 'column',
+                                'justify-content': 'space-evenly',
+                                'align-items': 'center',
+                                'margin': '0',
+                                'padding': '0',
+                                'width': '100%',
+                                'margin-bottom': '3rem',
+                                'background-color':'#161a28',
+                                'border': '#1e2130 solid 0.2rem' 
+                            },
+
                             children=[
-                                html.H5(
-                                    ['Total valores'], 
+
+                                html.Div(
+                                    className='inner-div',
                                     style={
-                                    'line-height': '1.6',
-                                    'box-sizing': 'border-box',
-                                    'margin': '1rem',
-                                    'font-weight': '500',
-                                    'align-self': 'flex-start',                                                
-                                    }
+                                        'margin': '0',
+                                        'padding': '0',
+                                        'width': '100%', 
+                                        'height': '350px',
+                                        'border-bottom': '#1e2130 solid 0.2rem'
+                                    },
+                                    
+                                    
+                                    # Gráfico
+                                    children=[
+                                        dcc.Graph(
+                                        id='grafico1',
+                                        figure=go.Figure(
+                                            data=[
+                                                go.Scatter( 
+                                                    y=Base_Dados.Quantidade, 
+                                                    x=Base_Dados.Data, 
+                                                    mode='lines', name='Gráfico 1',
+                                                    line=dict(color='orangered', width=3)
+                                                )],
+                                            layout=go.Layout(
+                                                title='Multas diárias',
+                                                template='plotly_dark',
+                                                plot_bgcolor='#161a28',
+                                                paper_bgcolor='#161a28',
+                                                font={'color': 'white'},
+                                                xaxis={'showgrid': False},
+                                                yaxis={'showgrid': False},
+                                                height=350, 
+                                                width=1050,
+                                                xaxis_title='Período',
+                                                yaxis_title='Quantidade'
+                                            ),
+                                            ),
+                                        ),
+                                    
+                                    ]
                                 ),
-                            
-                        
 
-                                html.H4(
-                                    [f'R$ { int( round( Base_Valor.Quantidade.sum() /1000000, 0 ) )  } mi'],
+                                html.Div(
+                                    className='inner-div',
                                     style={
-                                        'font-size': '1.5em',
-                                        'line-height': '1.6',
-                                        'font-weight': '400',
-                                        'color': '#3a8c5d',
-                                        'border-radius': '3px',
-                                        'padding': '12px 8px 12px 14px',
-                                        'border': '1px solid #D3D3D3',
-                                        'background': '#1e2130',
-                                        'box-sizing': 'border-box',
-                                        'width': '100%',
-                                        'display': 'flex',
-                                        'flex-direction': 'row',
-                                        'justify-content': 'center',
-                                    }
-                                )
-                            ]     
+                                        'margin': '0',
+                                        'padding': '0',
+                                        'width': '100%', 
+                                        'height': '350px',
+                                        'border-top': '#1e2130 solid 0.2rem'
+                                    },
+
+                                    children=[
+                                        dcc.Graph(
+                                        id='grafico2',
+                                        figure=go.Figure(
+                                            data=[
+                                                go.Bar( 
+                                                    y=Base_Valor['Quantidade'], 
+                                                    x=['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'], 
+                                                    # Marcador
+                                                    marker=dict(color='#3a8c5d'),
+                                                    #text=Base_Valor['Quantidade'] / 100000,
+                                                    textposition='outside',
+                                                    #hovertemplate='%{text:,.2f}',
+                                                    )],
+                                            layout=go.Layout(
+                                                title='Valores mensais aplicados',
+                                                template='plotly_dark',
+                                                plot_bgcolor='#161a28',
+                                                paper_bgcolor='#161a28',
+                                                font={'color': 'white'},
+                                                xaxis={'showgrid': False},
+                                                yaxis={'showgrid': False},
+                                                height=300, 
+                                                width=1050,
+                                                xaxis_title='Período',
+                                                yaxis_title='Valor em R$'
+                                            ),
+                                            ),
+                                        ),
+                                    ]
+                                ),
+                            ]
                         )
-
-
-
-
-
-
-
-
-
-
-
-                    ],
-                ),
-           ],
+                    ]
+                )
+            ],
         )
-
-    ]
-
+    ],
+    # Cor de fundo do projeto
+    style={'backgroundColor': '#1e2130'}
 )
 
 #ligar o servidor
